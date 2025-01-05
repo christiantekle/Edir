ce4b6225520dc3bad65e4c0a0af70c0b2755400c
Christian Awano <65189319+christiantekle@users.noreply.github.com>
2025-01-05 10:56:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import UserList from "./components/UserList";
 const App = () => {
   return (
     <div className="container mt-5">
-      <h1 className="text-center">Edir Management Tool</h1>
+      <h1 className="text-center">Hibret Be Geta</h1>
       <UserList />
     </div>
   );
